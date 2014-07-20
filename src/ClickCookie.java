b04c3218341f99067982730fc079b7f8d8d29765
james.park <jlp277@cornell.edu>
2014-07-20 00:35:16 -0700

    First implementation of cookie-clicker.

---
+++ src/ClickCookie.java
@@ -0,0 +1,75 @@
+import java.util.regex.Pattern;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
+
+
+public class ClickCookie {
+	private static int highproduct = 0;
+	private static JavascriptExecutor jse;
+	private static FirefoxDriver driver;
+
+	public static void main(String[] args) {
+		driver = new FirefoxDriver();
+		jse = (JavascriptExecutor) driver;
+		driver.get("http://orteil.dashnet.org/cookieclicker/");
+		while (true) {
+			// find highest item
+			// get cookie per second
+			// check if its worth it to buy next product
+			updateHighProduct();
+			driver.findElement(By.id("product"+ bestProdToBuy())).click();
+			// buy next product
+			int i;
+			for (i = 0; i <= 200; i++) {
+				driver.findElement(By.id("bigCookie")).click();
+			}
+		}
+	}
+
+	private static void updateHighProduct() {
+		int i = highproduct + 1;
+		System.out.println("i " + i);
+		String jsQuery = "return Game.ObjectsById[" + (highproduct + 1) + "].locked";
+		int islocked = Integer.parseInt(jse.executeScript(jsQuery).toString());
+		if (islocked == 0) {
+			System.out.println("highproduct: " + highproduct);
+			highproduct++;
+		}
+	}
+
+	private static int bestProdToBuy() {
+		int i = highproduct;
+		while (isHigherWorthIt(i)) {
+			i--;
+		}
+//		System.out.println("best to buy: " + i);
+		return i;
+	}
+
+	private static boolean isHigherWorthIt(int i) {
+		float hiprice = getPrice(i + 1);
+		float hicooksec = getCookiePerSecond(i + 1);
+		float loprice = getPrice(i);
+		float locooksec = getCookiePerSecond(i);
+//		System.out.println("higher worth it: " + ((hiprice/hicooksec) < (loprice/locooksec)));
+		return (hiprice/hicooksec) < (loprice/locooksec);
+	}
+
+	private static float getCookiePerSecond(int prodno) {
+		// the cookies per second of product numbered <prodno>
+		String jsQuery="return Game.ObjectsById[" + prodno + "].storedTotalCps*Game.globalCpsMult";
+		return Float.parseFloat(jse.executeScript(jsQuery).toString());
+	}
+
+	private static int getPrice(int prodno) {
+		String jsQuery = "return Game.ObjectsById[" + prodno + "].name";
+//		System.out.println("name" + jse.executeScript(jsQuery).toString());
+		jsQuery = "return Game.ObjectsById[" + prodno + "].price";
+		return Integer.parseInt(jse.executeScript(jsQuery).toString());
+	}
+}
